65899aa2ffe34dd07b242f1528e61fb46c9639db
devm <m@kos.co.th>
2025-06-28 15:16:36 +0700

    no need font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import { Footer, Header } from '@/components/layout';
 import { barlow } from '@/lib/fonts';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Digital Agency - Transform Your Business',
@@ -49,7 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${barlow.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${barlow.variable} antialiased`}
       >
         <Header />
         <main>{children}</main>
